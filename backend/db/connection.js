c53c7a16e6d3189efd057c7439b21487141561dd
Manjunath Magadum <magadummanjunath65@gmail.com>
2023-10-21 21:14:34 +0530

    added

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config(); // Load environment variables from .env file
 
 const connection = mysql.createConnection({
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port:process.env.DB_PORT
 });
 
 connection.connect((err) => {
