2a7b717047e2c3ae1d3bef837eaf3a0e4f105025
Navid <navidmulla60@gmail.com>
2023-10-21 18:14:52 +0530

    added server

---
+++ backend/db/connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+require('dotenv').config(); // Load environment variables from .env file
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+module.exports = connection;
---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./db/connection');
+
+app.get('/', (req, res) => {
+  db.query('SELECT * FROM test_table', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
